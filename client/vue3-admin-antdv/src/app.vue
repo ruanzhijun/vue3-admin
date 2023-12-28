8d59ce2e7ce218c09abdc3e4579d1d7e6e71eff3
ruanzhijun <ruanzhijun@ruanzhijun.cn>
2023-12-28 13:08:17 +0800

    完成demo
    Signed-off-by: ruanzhijun <ruanzhijun@ruanzhijun.cn>

--- client/vue3-admin-antdv/src/app.vue
+++ client/vue3-admin-antdv/src/app.vue
@@ -1,11 +1,8 @@
 <template>
   <ConfigProvider :locale="locale">
     <RouterView v-slot="{Component}">
-    <KeepAlive>
-      <component :is="Component" :key="$route.name" v-if="$route.meta.keep"></component>
-    </KeepAlive>
-    <component :is="Component" :key="$route.name" v-if="!$route.meta.keep"></component>
-  </RouterView>
+      <component :is="Component" :key="$route.name"></component>
+    </RouterView>
   </ConfigProvider>
 </template>
 
@@ -13,6 +10,7 @@
 import {Watermark} from '@pansy/watermark'
 import {ConfigProvider} from 'ant-design-vue'
 import zhCN from 'ant-design-vue/es/locale/zh_CN'
+
 new Watermark({text: 'vue3-admin-antdv', zIndex: 99999, container: 'app', blindOpacity: 1}).show()
 const visible = true
 const locale = zhCN
