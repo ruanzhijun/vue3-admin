9295cfef05f80bcd04a39277ff98499c136d43ba
ruanzhijun <ruanzhijun@ruanzhijun.cn>
2024-04-02 14:11:56 +0800

    新增路由缓存功能
    Signed-off-by: ruanzhijun <ruanzhijun@ruanzhijun.cn>

--- client/vue3-admin-antdv/src/app.vue
+++ client/vue3-admin-antdv/src/app.vue
@@ -1,8 +1,6 @@
 <template>
   <ConfigProvider :locale="locale">
-    <RouterView v-slot="{Component}">
-      <component :is="Component" :key="$route.name"></component>
-    </RouterView>
+    <RouterView/>
   </ConfigProvider>
 </template>
 
@@ -10,8 +8,14 @@
 import {Watermark} from '@pansy/watermark'
 import {ConfigProvider} from 'ant-design-vue'
 import zhCN from 'ant-design-vue/es/locale/zh_CN'
+import {onMounted} from 'vue'
 
 new Watermark({text: 'vue3-admin-antdv', zIndex: 99999, container: 'app', blindOpacity: 1}).show()
 const visible = true
 const locale = zhCN
+onMounted(() => {
+  // 禁止网页复制和右键功能
+  // document.addEventListener('selectstart', event => event.preventDefault())
+  // document.addEventListener('contextmenu', event => event.preventDefault())
+})
 </script>
